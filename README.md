06cf440a6c998dd9e377059d246dca0b310a1412
00nx6 <lszabo958@gmail.com>
2024-02-07 21:39:45 +0100

    wrote smth akin to pseudocode for most of the game

--- README.md
+++ README.md
@@ -36,3 +36,125 @@
 - Perks?
  - resistances
  - skills
+
+
+## Pseudo code
+On startup, the user is asked to pick a class, given a showing of their stats and starting weapons. It is explained that the classes not picked will be given as team mates.
+the user is asked to enter a short prompt for the story scenario, or leave blank for gpt to freestyle. 
+the prompt as `system` is sent to the openAi API.
+   The prompt specifies:
+      - what kind of weapons are in the game
+      - what the current level of the part is
+      - what kind of story it should generate
+      - return the same dict as fed but properly populated
+      - there is 1 player and 3 mute companion npcs
+         - cannot be interacted with
+      - try to steer the situation towards combat
+      - feel free to add other stuff to make it more interesting if you have time
+      
+Once the response is received the main title is displayed
+and the displayed story gets updated.
+the response contains actions, each action will be rendered as a button under the story
+if the dict contains `enemies`, a class called `Combat` will take effect where we do turn based attacks against the monster.
+<!-- 
+   maybe remove friendly npcs? we want to focus more on the combat and exploration aka. `the fun bits` 
+-->
+if the dict contains `friendly_npcs` their interactivity options get presented under the main story
+if the dict contains `interact` items they should be presented as options to interact with
+   aka display them as prompts for the user to interact with under the main story.
+   when selected, it will return a pre-written prompt for the bot
+   e.g:
+      `The player pulled the lever on the wall.`
+      `The player opened the chest and found an item.`
+
+continue game loop
+   
+__Combat__:
+Combat is a class that takes the enemies from the dict and the player
+with his companions and pauses the story. (meaning no more calls until the battle is decided)
+
+Enemies will be made into a subclass of enemy having:
+   - hp: int
+   - dmg: text
+   - def: int
+damage is given as `1d6 + 2` translating to 1 dice roll of a 6 sided dice + 2 as a base damage. 
+
+If damage done doesn't exceed def then attack will miss.
+there is a chance for the attack to completely miss.
+there is a chance for the attack to register as a crit (2x damage).
+This is for Sem to figure out how to make fair.
+
+Having 3 comrades, they will also each attempt an attack with the same rules.
+Upon victory or enemy slain a weapon upgrade is made.
+displays a cool picture, ripped straight off google search.
+
+__weapon__:
+Contains weapon 
+   - name 
+   - durability
+   - damage formulae 
+   - link to icon (link to local storage of small png)
+
+__companions__:
+Base mute companions.
+Has inventory, random weapons inside based on team level.
+They have an appropriate amount of health, defense based on team level. Ask Sem.
+Sends current weapon to companions subclass, receives damage value based on its formula. Ask sem
+
+__party__:
+Contains the player and companions.
+Mainly manages turns in combat for the npcs
+Also keeps track of party level. 
+This decides How strong companions are.
+The amount of enemies to be slain before a party level up is as follows:
+Level: number_of_enemies
+   - 1: 1
+   - 2: 2
+   - 3: 4
+   - 4: 8
+   - 5: 10
+max cap: 10 enemies
+Also influences enemy strength, higher level party = higher level enemies.
+
+__player__:
+Should ideally be a class
+Has a name, inventory, and a class. ask sem
+when performing an attack a weapon is selected from the inventory if there is 1 available
+that weapon is passed onto the player class subclass
+receives damage from players class subclass
+
+__player class__:
+subclass of player
+receives weapon details from the player class
+calculates damage
+returns damage to player
+contains the players health value, damage formula and defense
+these change based on level, appropriate values need to be decided.
+Available classes: 
+   - Figher
+   - Mage
+   - Barbarian
+   - Rogue
+
+__monsters__:
+Should ideally be a class
+Has a name, inventory, and a class. ask sem
+when performing an attack a weapon is selected from the inventory if there is 1 available
+that weapon is passed onto the monster type subclass
+receives damage from monster type subclass
+
+__monster type__:
+subclass of monster
+receives weapon details or modifiers from the monster class
+calculates damage
+returns damage to monster
+contains the monster health value, damage formula and defense
+these change based on level, appropriate values need to be decided.
+Available enemy types: 
+   - goblin
+   - orc
+   - dragon
+   - ask sem
+
+
+
---
+++ app.py
@@ -0,0 +1,114 @@
+from flask import Flask, render_template, request, redirect
+from random import randint
+
+app = Flask(__name__)
+
+app.config['TEMPLATE_AUTO_RELOAD'] = True
+
+"""
+what i have so far for the prompt:
+
+I have created a dictionary to store information for a DND like story game, the contents of this will be used according to the comments left in the dictionary. you will return it in the exact same format.
+
+Here is a bit more information about the contents and how it has to be populated:
+the `page_title` refers to a main title that will be displayed at the top of the screen, the next obj in the list will have most of the information,
+the title will refer to the current chapter title,
+description will be where the story is portrayed.
+When there is a`variable_type: int` given as a value, the expected value will be returned as a single int for that entry.
+When there is a `format`, it should be a string in the format provided. Otherwise everything will be explained in the comments in the dictionary. I will paste it after this message. Populate it as if we were playing an actual game of DND and thats how you stored the information.
+
+--dict
+
+the entries 'enemies' , 'friendly_npc'  and 'interact'  only need to be populated when they are a part of the story, so when youre talking to an character, they should be in the friendly_npc list, or if youre in battle the enemies youre fighting should be in the enemies list or if there are no items mentioned in the story the `interact` section should be left empty. Only populate `enemies`, `friendly_npc` and `interact` when the entries were mentioned by name in the story.
+
+you will populate it as if you were a game master in this game and this is how you stored the information
+
+from this point on i will only enter one of the options provided in the main story, can you keep generating dictionaries based on the options i pick?
+"""
+
+
+ai_response = {
+    'page_title': 'Usually referring to the current story arc or location',
+    'chapter': {
+        # will be displayed to the user.
+        'title': 'current story chapter',
+        # contains a short story
+        'story': 'max 150 words',
+        # options to move forward in the story.
+        'story_actions': {
+            'npc_option': [
+                'example: Purchase offered goods', 'example: Accept Quest'
+                ],
+                'enemy_option': [
+                    'example: attack enemy', 'example: loot corpse'
+                ],
+                'entity_options': [
+                    'example: pull lever', 'example: open chest'
+                ]
+            },
+        # list of enemies in combat with
+        # only populate if mentioned by name in the story
+        'enemies': [
+            {
+                'enemy_name': 'Variable_type: text',
+                'health': 'variable_type: int',
+                'defense': 'Variable_type: int',
+                'damage': 'format: 1d6+2, Variable_type:string',
+                'inventory': 'weapon name',
+                'attacks': {
+                    # im still not sure how attacks work
+                    # but please fill in the blanks
+                    'attack 1': 'attack name'
+                },
+            },
+            # ...
+        ],
+        # list of friendly_npcs in the vicinity
+        # only refers to quest givers traders and usually interact-able town NPCs
+        # only populate if mentioned by name in the story
+        'friendly_npc': [
+            {
+                'name': 'variable_type; text',
+                'race': 'variable_type: text',
+                'type': 'variable_type: text, example: Elf',
+                'role': 'merchant',
+                'has_quest': 'variable_type: bool, if has quest for player true else false',
+                'items_for_sale': [
+                    'item1', 'item2'
+                ]
+            },
+            {
+                'name': 'variable_type; text',
+                'race': 'variable_type: text',
+                'type': 'variable_type: text, example: Dark Elf',
+                'role': 'Variable_type: text, example: Tyrant king of Nilfgaard',
+                'has_quest': 'variable_type: bool, if has quest for player true else false'
+                
+            }
+        ],
+        # objects that can be interacted with in the nearby vicinity
+        # should be used in exploration parts of the story
+        # can be left blank in combat situations/exploration situations
+        # only populate if mentioned by name in the story
+        
+        'interact': [
+            {
+                'name': 'chest',
+                'type': 'object',
+            },
+            {
+                'name': 'lever',
+                'type': 'object',
+                'consequence': 'variable_type: string, example: Pull the lever and the door opens.'
+            }
+        ]
+    }
+}
+
+
+"""
+initial prompt has to specify:
+    - specify the weapons in the game
+    - types of monsters
+    
+"""
