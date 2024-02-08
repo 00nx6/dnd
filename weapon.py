a4384b6bab227b9c8736f0b1f9cd90e627cea3ee
Noah <554456@student.saxion.nl>
2024-02-08 13:09:18 +0100

    Weapons changed

--- weapon.py
+++ weapon.py
@@ -1,36 +1,37 @@
-player_weapons = {
-    'Fighter': {
-        'level 1': {'name': 'Dagger', 'damage': '1d4', 'modifier': 2},
-        'level 2': {'name': 'Shortsword', 'damage': '1d4', 'modifier': 2},
-        'level 3': {'name': 'Longsword', 'damage': '1d12', 'modifier': 0},
-        'level 4': {'name': 'Battleaxe', 'damage': '1d4', 'modifier': 0},
-        'level 5': {'name': 'Greatsword', 'damage': '1d4', 'modifier': 0},
-    },
+def player_weapons():
+    return {
+        'Fighter': {
+            'level 1': {'name': 'Dagger', 'damage': '1d4', 'modifier': 2},
+            'level 2': {'name': 'Shortsword', 'damage': '1d4', 'modifier': 2},
+            'level 3': {'name': 'Longsword', 'damage': '1d12', 'modifier': 0},
+            'level 4': {'name': 'Battleaxe', 'damage': '1d4', 'modifier': 0},
+            'level 5': {'name': 'Greatsword', 'damage': '1d4', 'modifier': 0},
+        },
 
-    'Mage': {
-        'level 1': {'name': 'Magic Missile', 'damage': '1d4', 'modifier': 2},
-        'level 2': {'name': 'Firebolt', 'damage': '1d4', 'modifier': 0},
-        'level 3': {'name': 'Lightning Bolt', 'damage': '3d4', 'modifier': 0},
-        'level 4': {'name': 'Frost Nova', 'damage': '1d4', 'modifier': 0},
-        'level 5': {'name': 'Meteor Swarm', 'damage': '1d4', 'modifier': 0},
-    },
+        'Mage': {
+            'level 1': {'name': 'Magic Missile', 'damage': '1d4', 'modifier': 2},
+            'level 2': {'name': 'Firebolt', 'damage': '1d4', 'modifier': 0},
+            'level 3': {'name': 'Lightning Bolt', 'damage': '3d4', 'modifier': 0},
+            'level 4': {'name': 'Frost Nova', 'damage': '1d4', 'modifier': 0},
+            'level 5': {'name': 'Meteor Swarm', 'damage': '1d4', 'modifier': 0},
+        },
 
-    'Barbarian': {
-        'level 1': {'name': 'Club', 'damage': '1d4', 'modifier': 2},
-        'level 2': {'name': 'Handaxe', 'damage': '1d6', 'modifier': 0},
-        'level 3': {'name': 'Greataxe', 'damage': '1d12', 'modifier': 1},
-        'level 4': {'name': 'Maul', 'damage': '1d4', 'modifier': 0},
-        'level 5': {'name': 'Halberd', 'damage': '1d4', 'modifier': 0},
-    },
+        'Barbarian': {
+            'level 1': {'name': 'Club', 'damage': '1d4', 'modifier': 2},
+            'level 2': {'name': 'Handaxe', 'damage': '1d6', 'modifier': 0},
+            'level 3': {'name': 'Greataxe', 'damage': '1d12', 'modifier': 1},
+            'level 4': {'name': 'Maul', 'damage': '1d4', 'modifier': 0},
+            'level 5': {'name': 'Halberd', 'damage': '1d4', 'modifier': 0},
+        },
 
-    'Rogue': {
-        'level 1': {'name': 'Shortbow', 'damage': '1d4', 'modifier': 2},
-        'level 2': {'name': 'Dagger', 'damage': '1d4', 'modifier': 0},
-        'level 3': {'name': 'Rapier', 'damage': '1d4', 'modifier': 4},
-        'level 4': {'name': 'Crossbow', 'damage': '1d4', 'modifier': 0},
-        'level 5': {'name': 'Poisoned Dagger', 'damage': '1d4', 'modifier': 0},
+        'Rogue': {
+            'level 1': {'name': 'Shortbow', 'damage': '1d4', 'modifier': 2},
+            'level 2': {'name': 'Dagger', 'damage': '1d4', 'modifier': 0},
+            'level 3': {'name': 'Rapier', 'damage': '1d4', 'modifier': 4},
+            'level 4': {'name': 'Crossbow', 'damage': '1d4', 'modifier': 0},
+            'level 5': {'name': 'Poisoned Dagger', 'damage': '1d4', 'modifier': 0},
+        }
     }
-}
 
 class Weapon:
     pass
