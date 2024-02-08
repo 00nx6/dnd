ffadde91509a693ca9871fb0861f465246e0f67c
Noah <554456@student.saxion.nl>
2024-02-08 14:45:14 +0100

    Changes monster class

--- monster.py
+++ monster.py
@@ -1,2 +1,19 @@
 class Monster:
-    pass
+    def __init__(self, name, hp, defense, weapon) -> None:
+        self.__name = name
+        self.__hp = hp
+        self.__defense = defense
+        self.__weapon = weapon
+
+    def get_name(self):
+        return self.__name    
+
+    def get_hp(self):
+        return self.__hp
+
+    def get_defense(self):
+        return self.__defense
+    
+    def get_weapon(self):
+        return self.__weapon
+
