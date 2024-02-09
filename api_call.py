b7f9c5ad7c64c27c6f4ce2360d173cbe61aef96c
00nx6 <lszabo958@gmail.com>
2024-02-09 19:43:37 +0100

    no more api

--- api_call.py
+++ api_call.py
@@ -1,143 +1,143 @@
 
-from openai import OpenAI
-import json
-from weapon import player_weapons
+# from openai import OpenAI
+# import json
+# from weapon import player_weapons
 
-from api_reader import ResponseHandler
+# from api_reader import ResponseHandler
 
-class OpenAIInterpreter:
-    def __init__(self) -> None:
+# class OpenAIInterpreter:
+#     def __init__(self) -> None:
 
-        # Configure the OpenAI API to work through our server (for payment credits)
-        self.openai = OpenAI(
-            api_key="dldr7bh6dp55",
-            base_url="https://openai.sd42.nl/api/providers/openai/v1"
-        )
+#         # # Configure the OpenAI API to work through our server (for payment credits)
+#         # self.openai = OpenAI(
+#         #     api_key="dldr7bh6dp55",
+#         #     base_url="https://openai.sd42.nl/api/providers/openai/v1"
+#         # )
 
-        self.ai_response = {
-            'page_title': 'Usually referring to the current story arc or location',
-            'chapter': {
-                # will be displayed to the user.
-                'title': 'current story chapter',
-                # contains a short story
-                'story': 'max 150 words',
-                # options to move forward in the story.
-                # list of enemies in combat with
-                # only populate if mentioned by name in the story
-                'enemies': [
-                    {
-                        'enemy_name': 'Variable_type: text',
-                        'health': 'variable_type: int',
-                        'defense': 'Variable_type: int',
-                        'damage': 'format: 1d6+2, Variable_type:string'
-                    },
-                    # ...
-                ]
-            }
-        }
-
-
-
-        self.prompt = f"""
-I have created a dictionary to store information for a DND like story game, the contents of this will be used according to the comments left in the dictionary. you will return it in the exact same format.
-
-Here is a bit more information about the contents and how it has to be populated:
-the `page_title` refers to a main title that will be displayed at the top of the screen, the next obj in the list will have most of the information,
-the title will refer to the current chapter title,
-description will be where the story is portrayed.
-When there is a`variable_type: int` given as a value, the expected value will be returned as a single int for that entry.
-When there is a `format`, it should be a string in the format provided. Otherwise everything will be explained in the comments in the dictionary. I will paste it after this message. Populate it as if we were playing an actual game of DND and thats how you stored the information.
-
-you will populate it as if you were a game master in this game and this is how you stored the information.
-
-the story should always lead to an encounter and face  a monster. We want enemies for 5 levels.. If they kill all the enemies and level up then they move on to a new area where a new encounter awaits. For each encounter give a random amount of monsters (1 to 4)
-
-the monster damage health and defense is based on these weapons, in one round of combat all 4 classes get one turn on the monster and the monster gets one turn too. damage is calculated by the damage of the player weapons - the monsters defense. An enemy should be killed in about 3 rounds. the campaign starts at level 1 and scale the monsters  later on when player levels also increase.
-
-These are the possible weapons:
-
-{json.dumps(player_weapons())}
-
-Here's an example response:
-
-{json.dumps(self.ai_response)}
-
-Make sure you respond with only a valid JSON object.
-"""
-
-        # Ask the LLM for a joke
-        self.response = self.openai.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": self.prompt}
-            ],
-        )
-
-    def get_opener(self):
-        return ResponseHandler(json.loads(str(self.response.choices[0].message.content)))
-
-    def get_next_chapter(self, level: int):
-        if level == 6:
-            return None
-
-        self.response = self.openai.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": f"We defeated the enemy and became level {level}, give us the next chapter of the story and new enemies. Here's an example response: {json.dumps(self.ai_response)}. Make sure you respond with only a valid JSON object."}
-            ],
-        )
-        return ResponseHandler(json.loads(str(self.response.choices[0].message.content)))
-            
-
-# if __name__ == "__main__":
-#     print(response)
-
-#     response_handled = json.loads(str(response.choices[0].message.content))
+#         self.ai_response = {
+#             'page_title': 'Usually referring to the current story arc or location',
+#             'chapter': {
+#                 # will be displayed to the user.
+#                 'title': 'current story chapter',
+#                 # contains a short story
+#                 'story': 'max 150 words',
+#                 # options to move forward in the story.
+#                 # list of enemies in combat with
+#                 # only populate if mentioned by name in the story
+#                 'enemies': [
+#                     {
+#                         'enemy_name': 'Variable_type: text',
+#                         'health': 'variable_type: int',
+#                         'defense': 'Variable_type: int',
+#                         'damage': 'format: 1d6+2, Variable_type:string'
+#                     },
+#                     # ...
+#                 ]
+#             }
+#         }
 
 
-#     # {
-#     #     "enemy_name": "Wolf Pack",
-#     #     "health": 20,
-#     #     "defense": 2,
-#     #     "damage": "1d6+2"
-#     # }
 
-#     r = ResponseHandler(response_handled)
-#     for enemy in r.get_chapter_enemies():
-#         m = Monster(enemy.get('enemy_name', "ERR"), enemy.get('health', '0'), enemy.get('defense', '99'), enemy.get('damage', '99d100+99999'))
-#         print(m.get_defense())
-#         i = 0
+#         self.prompt = f"""
+# I have created a dictionary to store information for a DND like story game, the contents of this will be used according to the comments left in the dictionary. you will return it in the exact same format.
 
-#     input("Win")
-#     level = 1
-#     while True:
-#         level += 1
+# Here is a bit more information about the contents and how it has to be populated:
+# the `page_title` refers to a main title that will be displayed at the top of the screen, the next obj in the list will have most of the information,
+# the title will refer to the current chapter title,
+# description will be where the story is portrayed.
+# When there is a`variable_type: int` given as a value, the expected value will be returned as a single int for that entry.
+# When there is a `format`, it should be a string in the format provided. Otherwise everything will be explained in the comments in the dictionary. I will paste it after this message. Populate it as if we were playing an actual game of DND and thats how you stored the information.
+
+# you will populate it as if you were a game master in this game and this is how you stored the information.
+
+# the story should always lead to an encounter and face  a monster. We want enemies for 5 levels.. If they kill all the enemies and level up then they move on to a new area where a new encounter awaits. For each encounter give a random amount of monsters (1 to 4)
+
+# the monster damage health and defense is based on these weapons, in one round of combat all 4 classes get one turn on the monster and the monster gets one turn too. damage is calculated by the damage of the player weapons - the monsters defense. An enemy should be killed in about 3 rounds. the campaign starts at level 1 and scale the monsters  later on when player levels also increase.
+
+# These are the possible weapons:
+
+# {json.dumps(player_weapons())}
+
+# Here's an example response:
+
+# {json.dumps(self.ai_response)}
+
+# Make sure you respond with only a valid JSON object.
+# """
+
+#         # # Ask the LLM for a joke
+#         # self.response = self.openai.chat.completions.create(
+#         #     model="gpt-3.5-turbo",
+#         #     messages=[
+#         #         {"role": "system", "content": self.prompt}
+#         #     ],
+#         # )
+
+#     def get_opener(self):
+#         return ResponseHandler(json.loads(str(self.response.choices[0].message.content)))
+
+#     def get_next_chapter(self, level: int):
 #         if level == 6:
-#             break
+#             return None
 
-#         response = openai.chat.completions.create(
+#         self.response = self.openai.chat.completions.create(
 #             model="gpt-3.5-turbo",
 #             messages=[
-#                 {"role": "system", "content": f"We defeated the enemy and became level {level}, give us the next chapter of the story and new enemies. Here's an example response: {json.dumps(ai_response)}. Make sure you respond with only a valid JSON object."}
+#                 {"role": "system", "content": f"We defeated the enemy and became level {level}, give us the next chapter of the story and new enemies. Here's an example response: {json.dumps(self.ai_response)}. Make sure you respond with only a valid JSON object."}
 #             ],
 #         )
+#         return ResponseHandler(json.loads(str(self.response.choices[0].message.content)))
+            
 
-#         response_handled = json.loads(str(response.choices[0].message.content))
-#         r = ResponseHandler(response_handled)
-#         for enemy in r.get_chapter_enemies():
-#             m = Monster(enemy.get('enemy_name', "ERR"), enemy.get('health', '0'), enemy.get('defense', '99'), enemy.get('damage', '99d100+99999'))
-#             print(m.get_defense())
-#             i = 0
+# # if __name__ == "__main__":
+# #     print(response)
+
+# #     response_handled = json.loads(str(response.choices[0].message.content))
+
+
+# #     # {
+# #     #     "enemy_name": "Wolf Pack",
+# #     #     "health": 20,
+# #     #     "defense": 2,
+# #     #     "damage": "1d6+2"
+# #     # }
+
+# #     r = ResponseHandler(response_handled)
+# #     for enemy in r.get_chapter_enemies():
+# #         m = Monster(enemy.get('enemy_name', "ERR"), enemy.get('health', '0'), enemy.get('defense', '99'), enemy.get('damage', '99d100+99999'))
+# #         print(m.get_defense())
+# #         i = 0
+
+# #     input("Win")
+# #     level = 1
+# #     while True:
+# #         level += 1
+# #         if level == 6:
+# #             break
+
+# #         response = openai.chat.completions.create(
+# #             model="gpt-3.5-turbo",
+# #             messages=[
+# #                 {"role": "system", "content": f"We defeated the enemy and became level {level}, give us the next chapter of the story and new enemies. Here's an example response: {json.dumps(ai_response)}. Make sure you respond with only a valid JSON object."}
+# #             ],
+# #         )
+
+# #         response_handled = json.loads(str(response.choices[0].message.content))
+# #         r = ResponseHandler(response_handled)
+# #         for enemy in r.get_chapter_enemies():
+# #             m = Monster(enemy.get('enemy_name', "ERR"), enemy.get('health', '0'), enemy.get('defense', '99'), enemy.get('damage', '99d100+99999'))
+# #             print(m.get_defense())
+# #             i = 0
     
-#         input("Win")
-# joke = response.choices[0].message.content
-# print(joke)
+# #         input("Win")
+# # joke = response.choices[0].message.content
+# # print(joke)
 
 
-# # Convert the joke to spoken audio and save it as an MP3 file
-# response = openai.audio.speech.create(model="tts-1", voice="alloy", input=joke)
-# response.stream_to_file("joke.mp3")
+# # # Convert the joke to spoken audio and save it as an MP3 file
+# # response = openai.audio.speech.create(model="tts-1", voice="alloy", input=joke)
+# # response.stream_to_file("joke.mp3")
 
-# # Create a DALL-E image for the joke
-# response = openai.images.generate(model="dall-e-3", prompt=joke, quality="standard", n=1, size="1024x1024")
-# urllib.request.urlretrieve(response.data[0].url, 'joke.png')
+# # # Create a DALL-E image for the joke
+# # response = openai.images.generate(model="dall-e-3", prompt=joke, quality="standard", n=1, size="1024x1024")
+# # urllib.request.urlretrieve(response.data[0].url, 'joke.png')
--- app.py
+++ app.py
@@ -2,14 +2,14 @@ from flask import Flask, render_template, redirect, request, url_for
 from player import Player as Pr
 from player import gen_lvl1_info, return_class_name_list
 from combat_handler import CombatHandler
-from api_call import OpenAIInterpreter
+# from api_call import OpenAIInterpreter
 from random import randint
 # rewowrk html generation
 # remodel based on new obj
 
 game_content = {}
 
-ai_interpreter = OpenAIInterpreter()
+# ai_interpreter = OpenAIInterpreter()
 
 app = Flask(__name__)
 app.config['TEMPLATE_AUTO_RELOAD'] = True
@@ -65,110 +65,33 @@ def story():
 
     player_class, npcs = init_class(user_name=user_name, player_subclass=player_subclass)
 
-    game_content['player'] = player_class
-    game_content['npcs'] = npcs
+    # game_content['player'] = player_class
+    # game_content['npcs'] = npcs
 
-    if 'ongoing' in game_content:
-        response = ai_interpreter.get_next_chapter(player_class.level)
-    else:
-        response = ai_interpreter.get_opener()
-        game_content['ongoing'] = True
+    # if 'ongoing' in game_content:
+    #     response = ai_interpreter.get_next_chapter(player_class.level)
+    # else:
+    #     response = ai_interpreter.get_opener()
+    #     game_content['ongoing'] = True
     
-    if response is None:
-        game_content['enemies'] = []
-        resp = {}
-    else:
-        game_content['enemies'] = response.get_chapter_enemies()
-        resp = response.get_ai_response()
+    # if response is None:
+    #     game_content['enemies'] = []
+    #     resp = {}
+    # else:
+    #     game_content['enemies'] = response.get_chapter_enemies()
+    #     resp = response.get_ai_response()
 
     return render_template('story.html',
-                           res=resp,
+                           res=ai_response,
                            player_class=player_class,
-                           nav_title=resp['page_title'],
+                           nav_title=ai_response['page_title'],
                            random=randint(1, 7)
                         )
 
 @app.route('/story/combat', methods=['GET'])
 def combat():
-    response = {
-    'page_title': 'Usually referring to the current story arc or location',
-    'chapter': {
-        # will be displayed to the user.
-        'title': 'current story chapter',
-        # contains a short story
-        'story': "Welcome, brave adventurers, to the mystical realm of Eldoria, where magic and monsters reign supreme. The land is cloaked in ancient mysteries, and a palpable tension hangs in the air as unseen forces vie for control. As you step into the bustling city of Eldoria's Crossroads, a mysterious letter arrives, beckoning you to the Moonlight Citadel. Rumors of a long-lost artifact, the Astral Keystone, have surfaced, and its rediscovery could tip the balance of power in the realm. Your party, a diverse group of warriors, mages, and rogues, has been chosen by fate to embark on this perilous quest. From the shadowy Whispering Woods to the fiery depths of the Dragon's Maw, you will face formidable challenges, forge alliances, and unravel the secrets that bind Eldoria. The fate of the realm rests in your hands, and as the sun sets on the horizon, your journey into the unknown begins. May your swords stay sharp, spells true, and courage unwavering in this epic Dungeons & Dragons adventure!",
-
-        # options to move forward in the story.
-        'story_actions': {
-            'npc_option': [
-                'example: Purchase offered goods', 'example: Accept Quest'
-                ],
-            'enemy_option': [
-                'example: attack enemy', 'example: loot corpse'
-                ],
-            'entity_options': [
-                'example: pull lever', 'example: open chest'
-                ]
-        },
-        # list of enemies in combat with
-        # only populate if mentioned by name in the story
-        'enemies': [
-            {
-                'enemy_name': 'Dog',
-                'health': 20,
-                'defense': 5,
-                'damage': '1d6+2',
-                'inventory': 'weapon name',
-                'attacks': {
-                    # im still not sure how attacks work
-                    # but please fill in the blanks
-                    'attack 1': 'attack name'
-                },
-            },
-            # ...
-        ],
-        # list of friendly_npcs in the vicinity
-        # only refers to quest givers traders and usually interact-able town NPCs
-        # only populate if mentioned by name in the story
-        'friendly_npc': [
-            {
-                'name': 'variable_type; text',
-                'race': 'variable_type: text',
-                'type': 'variable_type: text, example: Elf',
-                'role': 'merchant',
-                'has_quest': 'variable_type: bool, if has quest for player true else false',
-                'items_for_sale': [
-                    'item1', 'item2'
-                ]
-            },
-            {
-                'name': 'variable_type; text',
-                'race': 'variable_type: text',
-                'type': 'variable_type: text, example: Dark Elf',
-                'role': 'Variable_type: text, example: Tyrant king of Nilfgaard',
-                'has_quest': 'variable_type: bool, if has quest for player true else false'
-                
-            }
-        ],
-        # objects that can be interacted with in the nearby vicinity
-        # should be used in exploration parts of the story
-        # can be left blank in combat situations/exploration situations
-        # only populate if mentioned by name in the story
-        'interact': [
-            {
-                'name': 'chest',
-                'type': 'object',
-            },
-            {
-                'name': 'lever',
-                'type': 'object',
-                'consequence': 'variable_type: string, example: Pull the lever and the door opens.'
-            }
-        ]
-    }
-}
     return render_template('combat.html',
-                           res=response,
+                           res=ai_response,
                            player_class=game_content['player'],
                            npcs=game_content['npcs'])
                            
@@ -196,19 +119,7 @@ ai_response = {
         'story': "Welcome, brave adventurers, to the mystical realm of Eldoria, where magic and monsters reign supreme. The land is cloaked in ancient mysteries, and a palpable tension hangs in the air as unseen forces vie for control. As you step into the bustling city of Eldoria's Crossroads, a mysterious letter arrives, beckoning you to the Moonlight Citadel. Rumors of a long-lost artifact, the Astral Keystone, have surfaced, and its rediscovery could tip the balance of power in the realm. Your party, a diverse group of warriors, mages, and rogues, has been chosen by fate to embark on this perilous quest. From the shadowy Whispering Woods to the fiery depths of the Dragon's Maw, you will face formidable challenges, forge alliances, and unravel the secrets that bind Eldoria. The fate of the realm rests in your hands, and as the sun sets on the horizon, your journey into the unknown begins. May your swords stay sharp, spells true, and courage unwavering in this epic Dungeons & Dragons adventure!",
 
         # options to move forward in the story.
-        'story_actions': {
-            'npc_option': [
-                'example: Purchase offered goods', 'example: Accept Quest'
-                ],
-            'enemy_option': [
-                'example: attack enemy', 'example: loot corpse'
-                ],
-            'entity_options': [
-                'example: pull lever', 'example: open chest'
-                ]
-        },
-        # list of enemies in combat with
-        # only populate if mentioned by name in the story
+        
         'enemies': [
             {
                 'enemy_name': 'Variable_type: text',
@@ -223,44 +134,6 @@ ai_response = {
                 },
             },
             # ...
-        ],
-        # list of friendly_npcs in the vicinity
-        # only refers to quest givers traders and usually interact-able town NPCs
-        # only populate if mentioned by name in the story
-        'friendly_npc': [
-            {
-                'name': 'variable_type; text',
-                'race': 'variable_type: text',
-                'type': 'variable_type: text, example: Elf',
-                'role': 'merchant',
-                'has_quest': 'variable_type: bool, if has quest for player true else false',
-                'items_for_sale': [
-                    'item1', 'item2'
-                ]
-            },
-            {
-                'name': 'variable_type; text',
-                'race': 'variable_type: text',
-                'type': 'variable_type: text, example: Dark Elf',
-                'role': 'Variable_type: text, example: Tyrant king of Nilfgaard',
-                'has_quest': 'variable_type: bool, if has quest for player true else false'
-                
-            }
-        ],
-        # objects that can be interacted with in the nearby vicinity
-        # should be used in exploration parts of the story
-        # can be left blank in combat situations/exploration situations
-        # only populate if mentioned by name in the story
-        'interact': [
-            {
-                'name': 'chest',
-                'type': 'object',
-            },
-            {
-                'name': 'lever',
-                'type': 'object',
-                'consequence': 'variable_type: string, example: Pull the lever and the door opens.'
-            }
         ]
     }
 }
